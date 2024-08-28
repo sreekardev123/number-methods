8b8fe6a77f3818168dd14f94ff9a722201dd1406
sreekardev123 <sreesaikaranam27@gmail.com>
2024-08-28 11:14:45 +0530

    add

---
+++ day1.js
@@ -0,0 +1,15 @@
+//  let arr=[12.345, 67.890, 34.567]
+// let x=arr.map((arr)=>arr.toFixed(2))
+// console.log(x)
+// let arr=['123abc', '456def', '789ghi']
+// let x= arr.map((arr)=>parseInt(arr))
+// console.log(x)
+//  let arr=[12, 34.56, 78, 90.12]
+//  let x=arr.map((arr) =>Number.isInteger(arr))
+//  console.log(x)
+//  let arr=['abc', 123, 'def', 456]
+//  let x=arr.map((arr) => isNaN(arr))
+//  console.log(x)
+// let arr=[123.456, 789.012, 345.678]
+// let x=arr.map((arr) => arr.toPrecision(4))
+// console.log(x)
